1926527b24af379044388d3a6fa924d1b4dc6740
kennynku <nkuatsanakeneuoe@gmail.com>
2023-11-30 11:19:51 -0800

    corrected

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
-export default function getResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
+export default function getFullResponseFromAPI(success) {
+  const promise = new Promise((resolve, reject) => {
     if (success) {
       resolve({
         status: 200,
-        body: 'success',
+        body: 'Success',
       });
-    } else {
-      reject(Error('The fake API is not working currently'));
     }
+    reject(new Error('The fake API is not working currently'));
   });
+  return promise;
 }
