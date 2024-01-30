6c8c4bcab7ac35141b4ebd6b2e33e2f46c4d188f
kennynku <nkuatsanakeneuoe@gmail.com>
2024-01-30 19:49:38 +0000

    minor changes

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,14 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	const name = process.stdin.read();
-	if (name) {
-		process.stdout.write(`Your name is: ${name}`);
-	}
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
 
