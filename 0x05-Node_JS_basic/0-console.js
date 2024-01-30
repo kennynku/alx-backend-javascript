141c1477db7f51c1a028a4a2aed9ba4c63591a2d
kennynku <nkuatsanakeneuoe@gmail.com>
2024-01-30 19:55:16 +0000

    console

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,4 @@
-/** 
- *Displays a message on the STDOUT.
- * @param {String} msg The message to display.
- * @author Keneuoe Nkuatsana <https://github.com/kennynku>
- */
-const displayMessage = (msg) => {
-  console.log(msg);
-};
+const displayMessage = (str) => console.log(str);
 
 module.exports = displayMessage;
+
